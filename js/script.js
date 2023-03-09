e75d8037f6a7357b0d8f9c1815712d7e2e3bbe93
nespolisimmaco <snespoli@yahoo.com>
2023-03-09 17:39:17 +0100

    Add: checks on inputs and ticket price calculations

--- js/script.js
+++ js/script.js
@@ -11,52 +11,56 @@
 let userName = "";
 let kilometres = 0;
 let age = "";
+// Messaggio per l'utente
+let message = "";
 // Quando viene cliccato il bottone conferma
 const submitBtn = document.getElementById("submit-button");
 submitBtn.addEventListener("click", function () {
+    // Nome dell'utente
     const userNameInput = document.getElementById("user-name");
     userName = userNameInput.value;
     console.log("Nome e Cognome: ", userName);
     
+    // Chilometri da percorrere
     const kilometresInput = document.getElementById("kilometres");
     kilometres = parseInt(kilometresInput.value);
     console.log("Kilometri: ", kilometres);
 
+    // Fascia d'età
     const ageInput = document.getElementById("user-age");
     age = ageInput.value;
     console.log("Età: ", age);
-})
 
-// Messaggio per l'utente
-let message = "";
-
-if(!isNaN(userName) && !isNaN(kilometres) && !isNaN(age)) {
-    // LOGICA
-    // Il prezzo del biglietto è definito in base ai chilometri (0.21€ al km)
-    let ticketPrice = 0.21 * kilometres;
-    console.log("Prezzo del biglietto:", ticketPrice);
-    // Sconto del 20% per < 18
-    const minorsDiscount = 0.2 * ticketPrice;
-    // Sconto del 40% per > 65
-    const eldersDiscount = 0.4 * ticketPrice;
-    console.log(minorsDiscount, eldersDiscount);
-    // Message
-
-    if(age < 18) {
-        ticketPrice -= minorsDiscount;
-        console.log("Prezzo con sconto minorenni", ticketPrice); 
-    }else if(age > 65) {
-        ticketPrice -= eldersDiscount;
-        console.log("Prezzo con sconto over 65:", ticketPrice);
-    }  
+    // Check sugli imput
+    if((userName != "") && !isNaN(kilometres) && (age != "")) {
+        // LOGICA
+        // Il prezzo del biglietto è definito in base ai chilometri (0.21€ al km)
+        let ticketPrice = 0.21 * kilometres;
+        console.log("Prezzo del biglietto:", ticketPrice);
+        // Sconto del 20% per minorenni
+        const minorsDiscount = 0.2 * ticketPrice;
+        // Sconto del 40% per over65
+        const eldersDiscount = 0.4 * ticketPrice;
+        console.log(minorsDiscount, eldersDiscount);
     
+        if(age === "minorenne") {
+            ticketPrice -= minorsDiscount;
+            console.log("Prezzo con sconto minorenni", ticketPrice); 
+        }else if(age === "over65") {
+            ticketPrice -= eldersDiscount;
+            console.log("Prezzo con sconto over 65:", ticketPrice);
+        }
+        message = "Buon Viaggio";
+        
+    } else {
+        message = "Errore! Inserisci i dati correttamente.";
+    }
     // OUTPUT
     // Prezzo finale con massimo due decimali
     let fixedPrice = ticketPrice.toFixed(2);
     document.getElementById("user-age").innerHTML += age;
     document.getElementById("kilometres").innerHTML += kilometres + "km";
-    document.getElementById("price").innerHTML += fixedPrice + "€";
-} else {
-    message = "Errore! Reinserisci i dati.";
-}
-document.getElementById("message").innerHTML = message;
+    document.querySelector("ticket-price").innerHTML += fixedPrice + "€";
+    document.getElementById("message").innerHTML = message;
+})
+
